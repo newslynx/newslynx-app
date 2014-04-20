4ab941e31cc817897e799272569fa4b052299884
mhkeller <michael.keller@gmail.com>
2014-04-20 19:38:42 -0400

    add spotted tail chart

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,21 @@
 (function(){
 
-  var router;
+	var router;
 
 	function bindHandlers(){
-    // Rail section switching
+		// Rail section switching
 		$('.rail-section').on('click', function(){
-      var data_section = $(this).attr('data-section');
-      router.navigate(data_section, {trigger: true});
-    });
-  }
+			var data_section = $(this).attr('data-section');
+			router.navigate(data_section, {trigger: true});
+		});
+	}
 
-  function switchSection(activeSection){
-    $('.rail-section-title').removeClass('active');
-    $('.content-section').removeClass('shown');
-    $('.rail-section[data-section="'+activeSection+'"]').find('.rail-section-title').addClass('active');
-    $('.content-section[data-section="'+activeSection+'"]').addClass('shown');
-  }
+	function switchSection(activeSection){
+		$('.rail-section-title').removeClass('active');
+		$('.content-section').removeClass('shown');
+		$('.rail-section[data-section="'+activeSection+'"]').find('.rail-section-title').addClass('active');
+		$('.content-section[data-section="'+activeSection+'"]').addClass('shown');
+	}
 
 	function handleRoute(){
 		var Router = Backbone.Router.extend({
@@ -25,13 +25,33 @@
 		});
 		router = new Router;
 		router.on('route:section', function(section) {
-      switchSection(section);
+			switchSection(section);
 		});
 			
 		// For bookmarkable Urls
 		Backbone.history.start();
 	}
 
+	function drawChart(data){
+		var formatDate = d3.time.format("%b %Y"),
+				legend = {
+					facebook: { metric: 'likes', color: "#3B5998" },
+					twitter: { metric: 'mentions', color: "#55ACEE" }
+				};
+
+		d3.select('#ST-chart')
+			.datum(data)
+			.call(spottedTail()
+				.x(function(d) { return formatDate.parse(d.date); })
+				.y(function(d) { return +d.count; })
+				.legend(legend))
+	}
+
+	function loadTimelineChart(){
+			d3.csv('data/dummy-timeline-counts.csv', drawChart)
+	}
+
+	loadTimelineChart();
 	handleRoute();
 	bindHandlers();
 
