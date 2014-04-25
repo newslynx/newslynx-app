6bf375daedf5b9c6e6ccf134ac5ba45b385c1413
mhkeller <michael.keller@gmail.com>
2014-04-25 00:44:36 -0400

    button styles, colors, event filtering

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,27 @@
 			var data_section = $(this).attr('data-section');
 			router.navigate(data_section, {trigger: true});
 		});
+
+		// Tag event filtering
+		$('.tag-filter').on('click', function(){
+			$(this).toggleClass('selected');
+			checkEventFilters();
+		})
+	}
+
+	function checkEventFilters(){
+		var selected_list = [];
+		$('.tag-filter').each(function(index, el){
+			var $el = $(el);
+			if ($el.hasClass('selected')) selected_list.push($el.attr('data-tag'))
+		});
+
+		console.log(selected_list)
+		$('.event-item-wrapper').hide();
+		for (var i = 0; i < selected_list.length; i++){
+
+			$('.event-item-wrapper[data-tag*="'+selected_list[i]+'"]').show()
+		}
 	}
 
 	function switchSection(activeSection){
@@ -33,11 +54,28 @@
 	}
 
 	function drawChart(data){
-		var formatDate = d3.time.format("%b %Y"),
-				legend = {
-					facebook: { metric: 'likes', color: "#3B5998" },
-					twitter: { metric: 'mentions', color: "#55ACEE" }
-				};
+		var formatDate = d3.time.format("%b %Y");
+		var legend =	{
+			facebook: {metric: 'likes', color: "#3B5998"},
+			twitter: {metric: 'mentions', color: "#55ACEE"}
+		}
+		var notes = [
+			{
+				date: 'Sep 2007',
+				text: 'A sample note',
+				type: ['note']
+			},
+			{
+				date: 'Jul 2005',
+				text: 'Major event',
+				type: ['major-event']
+			},
+			{
+				date: 'Aug 2006',
+				text: 'Citation event',
+				type: ['citation']
+			}
+		]
 
 		d3.select('#ST-chart')
 			.datum(data)
@@ -45,11 +83,12 @@
 				.x(function(d) { return formatDate.parse(d.date); })
 				.y(function(d) { return +d.count; })
 				.margin({right: 20})
+				.notes(notes)
 				.legend(legend))
 	}
 
 	function loadTimelineChart(){
-			d3.csv('data/dummy-timeline-counts.csv', drawChart)
+		d3.csv('data/dummy-timeline-counts.csv', drawChart)
 	}
 
 	loadTimelineChart();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var app = require('../app');
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -10,6 +10,25 @@ router.get('/', function(req, res) {
 router.get('/article', function(req, res){
   var opts = {};
   opts.title = 'Article page';
+  opts.tags = ['social', 'major-event', 'citation']
+  opts.events = [
+		{
+			"title": "event1",
+			"description": "Lorem ipsum doler sit amet consectueuer.",
+			"tags": ['social', 'major-event']
+		},
+		{
+			"title": "event2",
+			"description": "The carbon in our apple pies billions upon billions cosmos.",
+			"tags": ['citation']
+		},
+		{
+			"title": "event3",
+			"description": "By gravity a very small stage in a vast cosmic arena a mote of dust suspended in a sunbeam.",
+			"tags": ['major-event']
+		}
+	]
+
   res.render('article', opts);
 });
 
