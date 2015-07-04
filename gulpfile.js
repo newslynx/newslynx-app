b8234fee2b6a4a718e424a7f9293e3b6cb023bc9
mhkeller <michael.keller@gmail.com>
2015-07-04 01:04:27 -0400

    use gulp to compile stylus and js

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var nib = require('nib');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+
+var paths = {
+  css: {
+    main: './lib/public/stylesheets/*.styl',
+    all: [
+      './lib/public/stylesheets/**/*.styl',
+      './lib/public/stylesheets/*.styl'
+    ]
+  },
+  js: [
+    './lib/public/javascripts/namespace.js',
+    './lib/public/javascripts/helpers/*.js',
+    './lib/public/javascripts/models/*.js',
+    './lib/public/javascripts/collections/*.js',
+    './lib/public/javascripts/app/*.js',
+    './lib/public/javascripts/views/*.js',
+    './lib/public/javascripts/routing/*.js',
+    './lib/public/javascripts/init.js'
+  ]
+};
+
+gulp.task('compile-stylus', function() {
+  // Stylus has an awkward and perplexing 'include css' option
+  gulp.src(paths.css.main)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib(),
+    }))
+    .pipe(gulp.dest('lib/public/stylesheets/css/'));
+});
+
+gulp.task('compile-js', function() {
+  return gulp.src(paths.js)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.bundled.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./lib/public/javascripts/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.css.all, ['compile-stylus'])
+  gulp.watch(paths.js, ['compile-js']);
+});
+
+gulp.task('default', ['watch', 'compile-stylus', 'compile-js']);
