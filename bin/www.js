6243cb5e1dd98869173aa62ff79e50655a36617d
mhkeller <michael.keller@gmail.com>
2015-07-19 22:43:07 -0400

    allow configurable hostname

--- bin/www.js
+++ bin/www.js
@@ -8,33 +8,42 @@ var debug = require('tracer').console();
 var chalk = require('chalk');
 
 
-function launchServers(port, cb){
+function launchServers(port, hostname, cb){
   port = port || process.env.NEWSLYNX_HTTP_PORT || 3000;
+  hostname = hostname || process.env.NEWSLYNX_HTTP_HOST || '0.0.0.0';
   app.set('http_port', port);
 
+  console.log(hostname)
+
   // Launch a server
-  var http_server = http.createServer(app).listen(port, function(err){
+  var http_server = http.createServer(app).listen(port, hostname, function(err){
     if (!err) {
-      console.log(chalk.green('##################################'));
-      console.log(chalk.green('# HTTP listening on port ' + port + '... #'));
-      console.log(chalk.green('##################################'));
+      console.log(chalk.green('#####################################'));
+      console.log(chalk.green('# HTTP listening on '+hostname+':' + port + '... #'));
+      console.log(chalk.green('#####################################'));
       
       if (cb) { cb(); }
     } else {
-      console.log(chalk.red('###############################'));
-      console.log(chalk.red('# HTTP failed on port ' + port + '... #'));
-      console.log(chalk.red('###############################'));
+      console.log(chalk.red('##################################'));
+      console.log(chalk.red('# HTTP failed on '+hostname+':' + port + '... #'));
+      console.log(chalk.red('##################################'));
     }
   });
 
 }
 
 // This file can both be run via the command line if given the proper flag or as a nodemodule
+/* Options, to be documented 
+  `port` and `hostname` are optional
+  node bin/www.js run <port> <hostname>
+*/
+
 var launch_command = process.argv[2],
     port = process.argv[3];
+    hostname = process.argv[4];
 
 if (launch_command === 'run'){
-  launchServers(port);
+  launchServers(port, hostname);
 } else if (launch_command){
   throw new Error('If you want to run the server from the command line, you must supply the `run` command: `./bin/www run`.\n')
 }
