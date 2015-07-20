834703edcac7ab164ed2f210be52f9914af67561
mhkeller <michael.keller@gmail.com>
2015-07-19 23:32:43 -0400

    fix travis test with hostname

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var newslynx = require('./bin/www.js');
 
 describe('runServer', function () {
  it('should run without error', function(done) {
-    newslynx.run(3333, function(err){
+    newslynx.run(3333, '0.0.0.0', function(err){
       if (err) {
         throw err
       }
